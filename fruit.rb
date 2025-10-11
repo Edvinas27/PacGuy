b9c77cc9c6f002e83a246f76d1b0d178b3fe30fe
Edvinas Montvilas <edvinas.montvilas@vinted.com>
2025-10-11 20:20:28 +0300

    Added general logic

---
+++ fruit.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class Fruit
+  attr_reader :x, :y, :type
+
+  def initialize(type, image_path)
+    @type = type
+    @image = Gosu::Image.new(image_path)
+    @x = 0
+    @y = WINDOW_HEIGHT/2
+    @speed = ITEM_SPEED + rand(3)
+    @dropping = false
+  end
+
+  def update
+    if @dropping
+      @y += @speed
+    else
+      @x += @speed
+    end
+  end
+
+  def draw
+    @image.draw(@x, @y, 1)
+  end
+
+  def off_screen?(window_width, window_height)
+    @x > window_width || @y > window_height
+  end
+
+  def start_dropping
+    @dropping = true
+  end
+end
--- main.rb
+++ main.rb
@@ -1,25 +1,54 @@
 # frozen_string_literal: true
 require 'gosu'
+require_relative 'fruit'
 
 TITLE = "Rusiuotojas"
+WINDOW_HEIGHT = 800
+WINDOW_WIDTH = 600
+ITEM_SPEED = 5
 
 class SorterGame < Gosu::Window
-  WINDOW_HEIGHT = 800
-  WINDOW_WIDTH = 600
-  ITEM_SPEED = 2
 
   def initialize
-    super(WINDOW_HEIGHT, WINDOW_WIDTH)
+    super(WINDOW_WIDTH,WINDOW_HEIGHT)
     self.caption = TITLE
     @background = Gosu::Image.new("images/background.jpg")
+    @active_fruits = []
+    @fruits =  [
+      { :apple => "images/fruits/Apple.png", },
+      { :banana => "images/fruits/Banana.png", },
+      { :grape => "images/fruits/Grape.png", }
+    ]
+    @spawn_timer = 0
   end
 
   def update
-
+    @active_fruits.each(&:update)
+    @active_fruits.reject! { |fruit| fruit.off_screen?(WINDOW_WIDTH, WINDOW_HEIGHT) }
   end
 
   def draw
     @background.draw(0,0,0,0.4,0.4)
+    @active_fruits.each(&:draw)
+  end
+
+  def button_down(id)
+    case id
+    when Gosu::KB_SPACE
+      if @active_fruits.any?
+        @active_fruits.last.start_dropping
+      else
+        spawn_fruit
+      end
+    end
+  end
+
+  private
+
+  def spawn_fruit
+    fruit_data = @fruits.sample
+    fruit, image_path = fruit_data.first
+    @active_fruits << Fruit.new(fruit, image_path)
   end
 end
 
